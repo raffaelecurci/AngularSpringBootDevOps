fc6181c8f5dcffc86aa7733444c829e7165f80a6
Raffa <you@example.com>
2020-11-17 11:43:34 +0100

    fixed db user creation

--- Dockerfile
+++ Dockerfile
@@ -2,23 +2,31 @@ FROM mongo
 
 RUN apt-get update
 RUN apt-get install -y openjdk-8-jdk
+RUN mkhomedir_helper mongodb
+RUN chsh --shell /bin/bash mongodb
+#RUN userdel --remove mongodb
+#RUN useradd -ms /bin/bash mongodb
 RUN mkdir -p /data/log
 RUN touch /var/log/mongodb.log
+RUN touch /home/mongodb/.dbshell
 RUN chown -R mongodb:mongodb /data
+RUN chown -R mongodb:mongodb /home/mongodb
 RUN chown -R mongodb:mongodb /var/log/mongodb.*
 RUN chmod 664 /var/log/mongodb.*
 RUN useradd -ms /bin/bash spring
 COPY backend/target/*.jar /home/spring/application.jar
 RUN chown spring:spring /home/spring/application.jar
 COPY mongoInit.sh mongoInit.sh
+
 RUN chmod +x mongoInit.sh
-RUN touch init.js
 RUN chown mongodb:mongodb mongoInit.sh
-RUN chown mongodb:mongodb init.js
+
+RUN touch /home/mongodb/init.js
+RUN chown mongodb:mongodb /home/mongodb/init.js
 
 EXPOSE 27017
 EXPOSE 8080
 #USER root:root
 #CMD ["mongod","--bind_ip_all","--fork","--logpath","/var/log/mongodb.log", "&&", "java","-jar","/home/spring/application.jar"]
 #ENTRYPOINT ["/bin/sh"]
-CMD ["./mongoInit.sh"]
+CMD ./mongoInit.sh
--- mongoInit.sh
+++ mongoInit.sh
@@ -2,7 +2,7 @@
 
 # Initialize a mongo data folder and logfile
 
-/bin/su -c "mongod --bind_ip 127.0.0.1 --logpath /var/log/mongodb.log --logappend &" - mongodb
+/bin/su -c "cd; pwd ; /usr/bin/mongod --bind_ip 127.0.0.1 --fork --logpath /var/log/mongodb.log --logappend &" - mongodb
 
 # Wait until mongo logs that it's ready (or timeout after 60s)
 COUNTER=0
@@ -14,11 +14,12 @@ while [[ $? -ne 0 && $COUNTER -lt 60 ]] ; do
     grep -q 'Waiting for connections' /var/log/mongodb.log
 done
 
-echo "use Employee" > init.js
-echo "db.createUser({	user: \"Employeeadmin\", pwd: \"password\",	roles:[{role: \"userAdmin\" , db:\"Employee\"}]})" >> init.js
+echo "use Employee" > /home/mongodb/init.js
+echo "db.createUser({	user: \"Employeeadmin\", pwd: \"password\", mechanisms:[\"SCRAM-SHA-1\"], roles:[{role: \"userAdmin\" , db:\"Employee\"}]})" >> /home/mongodb/init.js
 
 #Initialize mongodb, create db and user
-/bin/su -c "mongo < init.js" - mongodb
+/bin/su -c "mongo < /home/mongodb/init.js" - mongodb
 
 #Launch Spring application
-/bin/su -c "java -jar /home/spring/application.jar >> ~/application.log" - spring
+/bin/su -c "cd ; pwd ; java -jar /home/spring/application.jar " - spring
+#>> ~/application.log &
